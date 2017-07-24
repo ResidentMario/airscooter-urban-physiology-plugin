44084f231e4df00ad0dbd9719091dd293d83400d
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2017-07-24 16:31:18 -0400

    Initial commit.

---
+++ airscooter_urban_physiology_plugin/urban_physiology_plugin.py
@@ -0,0 +1,14 @@
+import click
+# noinspection PyUnresolvedReferences
+import urban_physiology_toolkit.workflow_utils as workflow_utils
+
+
+@click.command()
+def init_catalog():
+    workflow_utils.init_catalog("glossary.json", ".")
+    workflow_utils.update_dag(root=".")
+
+
+@click.command()
+def finalize():
+    raise NotImplementedError("Catalog finalization has not been implemented yet.")
