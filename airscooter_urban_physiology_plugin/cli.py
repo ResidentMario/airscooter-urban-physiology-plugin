d5861b9835892acfcceb3d0e54428f1c06c0b050
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2017-07-26 14:03:19 -0400

    Add filesize and column rate limiting to init_catalog.

--- airscooter_urban_physiology_plugin/cli.py
+++ airscooter_urban_physiology_plugin/cli.py
@@ -4,8 +4,12 @@ import urban_physiology_toolkit.workflow_utils as workflow_utils
 
 
 @click.command()
-def init_catalog():
-    workflow_utils.init_catalog("glossary.json", ".")
+@click.option('--max-filesize', help='The maximum size (in MB) of resources in the glossary that will be read into the '
+                                     'catalog.')
+@click.option('--max-columns', help='The maximum size (in number of columns of data) of resources in the glossary that '
+                                    'will be read into the catalog.')
+def init_catalog(max_filesize, max_columns):
+    workflow_utils.init_catalog("glossary.json", ".", max_filesize=max_filesize, max_columns=max_columns)
     workflow_utils.update_dag(root=".")
 
 
